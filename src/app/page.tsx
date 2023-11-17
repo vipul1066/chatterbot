007b0a8d37132330e1638e8a473266d0414bf2e1
vipul1066 <109271828+vipul1066@users.noreply.github.com>
2023-11-17 16:53:11 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,47 +45,44 @@ export default function Home() {
     }
   };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-24 py-5">
-      <h1 className="text-5xl font-sans">ChatterBot</h1>
+<main className="flex min-h-screen flex-col items-center justify-between px-8 py-5 bg-gradient-to-b from-blue-600 to-blue-800 text-white">
+      <h1 className="text-5xl font-semibold mb-8">ChatterBot</h1>
 
-      <div className="flex  h-[35rem] w-[40rem] flex-col items-center bg-gray-600 rounded-xl">
-        <div className=" h-full flex flex-col gap-2 overflow-y-auto py-8 px-3 w-full">
-          {messages.map((e) => {
-            return (
-              <div
-                key={e.content}
-                className={`w-max max-w-[18rem] rounded-md px-4 py-3 h-min ${
-                  e.role === "assistant"
-                    ? "self-start  bg-gray-200 text-gray-800"
-                    : "self-end  bg-gray-800 text-gray-50"
-                } `}
-              >
-                {e.content}
-              </div>
-            );
-          })}
-
-
-
-          {isLoading ? <div className="self-start  bg-gray-200 text-gray-800 w-max max-w-[18rem] rounded-md px-4 py-3 h-min">*loading*</div> : ""}
+      <div className="flex h-[35rem] w-[40rem] flex-col items-center bg-gray-800 rounded-xl shadow-lg">
+        <div className="h-full flex flex-col gap-2 overflow-y-auto py-8 px-4 w-full">
+          {messages.map((e, index) => (
+            <div
+              key={index}
+              className={`w-max max-w-[18rem] rounded-md px-4 py-3 h-min ${
+                e.role === 'assistant'
+                  ? 'self-start bg-gray-200 text-gray-800'
+                  : 'self-end bg-gray-600 text-gray-50'
+              }`}
+            >
+              {e.content}
+            </div>
+          ))}
+          {isLoading && (
+            <div className="self-start bg-gray-200 text-gray-800 w-max max-w-[18rem] rounded-md px-4 py-3 h-min animate-pulse">
+              thinking
+            </div>
+          )}
         </div>
-        <div className="relative  w-[80%] bottom-4 flex justify-center">
+        <div className="relative w-full flex justify-center">
           <textarea
             value={theInput}
             onChange={(event) => setTheInput(event.target.value)}
-            className="w-[85%] h-10 px-3 py-2 resize-none overflow-y-auto text-black bg-gray-300 rouded"
-            onKeyDown={Submit}
+            className="w-[85%] h-10 px-3 py-2 resize-none overflow-y-auto text-black bg-gray-300 rounded-l outline-none focus:ring focus:border-blue-300"
+            onKeyDown={handleSubmit}
           />
           <button
             onClick={callGetResponse}
-            className="w-[15%] bg-blue-500 px-4 py-2 rounded-sm "
+            className="w-[15%] bg-blue-500 px-4 py-2 rounded-r transition-transform transform hover:scale-105 focus:outline-none focus:ring focus:border-blue-300"
           >
-            send
+            Send
           </button>
         </div>
       </div>
-
-      <div></div>
     </main>
   );
-}
+};
